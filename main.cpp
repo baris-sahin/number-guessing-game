53e7b3073556bd6ea440165d6935dbf0188c4718
Barış Şahin <93740263+baris-sahin@users.noreply.github.com>
2021-11-05 11:52:22 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char **argv)
 {
     int temp,x;
-    std::cout << "\t\t\tHow to Play\n";
+    std::cout << "\t\t\t\tHow to Play\n";
     std::cout << "The game requires the player and the computer to choose a 4-digit and all different numbers and" <<
     " the computer and the player has to find them via two types of clues that two sides give each other after each guess." <<
     " If the guess includes a number that are in the digits, the - parameter gains +1, if the guess includes a number that are in" <<
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
     " and the computer race each other to find the correct number with the least amount of tries." <<
     " Whichever side finds the number with the least amount of guesses wins the game." << std::endl;
     label1:
-    std::cout << "Enter 1 for guess computer's number and enter 2 for test the computer decision capability. To exit enter -1." << std::endl;
+    std::cout << "\n \nEnter 1 for guess computer's number and enter 2 for test the computer decision capability. To exit enter -1." << std::endl;
     std::cin >> temp;
 
     test obj;
